b6c23c3dc93a6776dc5251684ab147719ed8607b
Replit user <replituser@example.com>
2021-03-24 18:56:26 +0000

    new try

--- list.c
+++ list.c
@@ -59,7 +59,7 @@ void * nextList(List * list) {
     list->current = nodo;
   }
   else return NULL;
-  //nodo = list->current->next;
+  list->current->next = nodo->next;
   return (void *) nodo->data;
 
   /*Node *nodo = list->head;

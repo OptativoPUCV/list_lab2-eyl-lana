f8a319709367085147e1bb54b702d1d575ed48fa
Replit user <replituser@example.com>
2021-03-24 19:35:19 +0000

    new try

--- list.c
+++ list.c
@@ -57,9 +57,9 @@ void * nextList(List * list) {
   Node *nodo = list->current;
   while (nodo != NULL){
     nodo = nodo->next;
+    list->current = nodo;
   }
   //else return NULL;
-  list->current = nodo;
 
   return (void *) nodo->data;
 
